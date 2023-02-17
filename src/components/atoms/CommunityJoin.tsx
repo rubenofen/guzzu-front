644d26a36f89ed998b127a622614d00a5533b996
Rubén Sahagún <rubenofen@gmail.com>
2023-02-17 14:42:54 +0100

    Join community with link

--- src/components/atoms/CommunityJoin.tsx
+++ src/components/atoms/CommunityJoin.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import communityJoin from 'public/images/community-join.png'
 import discord from 'public/images/discord.gif'
 import rule from 'public/images/rule.png'
@@ -9,20 +11,25 @@ type CommunityJoinProps = {
   className?: string
 }
 
-export const CommunityJoin = ({ className }: CommunityJoinProps) => (
-  <div className={`relative flex w-full h-full ${className}`}>
-    <Image src={communityJoin} alt="community join" />
-    <div className="flex flex-col w-full">
-      <div className="p-6">
-        <h2 className="font-normal">JOIN OUR COMMUNITY AND BE AWARE OF OUR LATEST NEWS</h2>
-        <Button className="btn-primary btn-big mt-4" icon={<AiOutlineArrowRight />}>
-          JOIN OUR DISCORD
-        </Button>
-      </div>
-      <div className="relative w-full h-full flex justify-center items-center">
-        <Image src={rule} alt="rule" fill className="object-cover" />
-        <Image src={discord} alt="discord" fill className="object-contain z-10" />
+export const CommunityJoin = ({ className }: CommunityJoinProps) => {
+  const router = useRouter()
+  return (
+    <div className={`relative flex w-full h-full ${className}`}>
+      <Image src={communityJoin} alt="community join" />
+      <div className="flex flex-col w-full">
+        <div className="p-6">
+          <h2 className="font-normal">JOIN OUR COMMUNITY AND BE AWARE OF OUR LATEST NEWS</h2>
+          <Link href="https://discord.com/invite/mckxHh2QCz" target="_blank">
+            <Button className="btn-primary btn-big mt-4" icon={<AiOutlineArrowRight />}>
+              JOIN OUR DISCORD
+            </Button>
+          </Link>
+        </div>
+        <div className="relative w-full h-full flex justify-center items-center">
+          <Image src={rule} alt="rule" fill className="object-cover" />
+          <Image src={discord} alt="discord" fill className="object-contain z-10" />
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
